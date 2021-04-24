1e5bd2fdd261ca8a417d9e2bde003d2a76dbcddd
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2021-04-24 19:14:47 +0000

    cleaning up

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,27 @@ import styles from '../styles/Home.module.css'
 import Action from '../components/action'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home resources planning</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <Action component="person" route="people" type="create" />
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    return <>
+            <Head>
+                <title>Home resources planning</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    Header
+                </div>
+                <div className={styles.left}>
+                    Left
+                </div>
+                <div className={styles.main}>
+                    <Action component="person" route="people" type="create" />
+                </div>
+                <div className={styles.right}>
+                    Right
+                </div>
+                <div className={styles.footer}>
+                    Footer
+                </div>
+            </div>
+        </>
 }
