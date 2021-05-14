9be020a16697c4a66c46dac9b85ae78c47007373
Eduardo Messias <eduardo.messias@outlook.com>
2021-05-14 10:13:56 +0100

    landing page - fix#6

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
